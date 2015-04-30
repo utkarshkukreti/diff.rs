34537ebdf9ac7bb84d88ad5d70642b83b128e9ac
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2015-04-30 12:43:52 +0530

    refactor(slice): use iterators instead of indexing the inputs
    This will make it easy to implement diffing things like chars, words,
    lines in a String in the future.

--- src/lib.rs
+++ src/lib.rs
@@ -9,10 +9,18 @@ pub enum Result<T> {
 /// Computes the diff between two slices.
 pub fn slice<'a, T: PartialEq>(left: &'a [T],
                                right: &'a [T]) -> Vec<Result<&'a T>> {
-    let mut table = vec![vec![0; right.len() + 1]; left.len() + 1];
+    iter(left.iter(), right.iter())
+}
 
-    for (i, l) in left.iter().enumerate() {
-        for (j, r) in right.iter().enumerate() {
+fn iter<'a, I, T>(left: I, right: I) -> Vec<Result<T>> where
+    I: Clone + Iterator<Item = T> + DoubleEndedIterator, T: PartialEq
+{
+    let left_count = left.clone().count();
+    let right_count = right.clone().count();
+    let mut table = vec![vec![0; right_count + 1]; left_count + 1];
+
+    for (i, l) in left.clone().enumerate() {
+        for (j, r) in right.clone().enumerate() {
             table[i+1][j+1] = if l == r {
                 table[i][j] + 1
             } else {
@@ -22,18 +30,20 @@ pub fn slice<'a, T: PartialEq>(left: &'a [T],
     }
 
     let mut diff = vec![];
-    let (mut i, mut j) = (left.len(), right.len());
+    let (mut i, mut j) = (left_count, right_count);
+    let (mut li, mut ri) = (left.clone(), right.clone());
     loop {
         if i > 0 && (j == 0 || table[i][j] == table[i-1][j]) {
             i -= 1;
-            diff.push(Result::Left(&left[i]));
+            diff.push(Result::Left(li.next_back().unwrap()));
         } else if j > 0 && (i == 0 || table[i][j] == table[i][j-1]) {
             j -= 1;
-            diff.push(Result::Right(&right[j]));
+            diff.push(Result::Right(ri.next_back().unwrap()));
         } else if i > 0 && j > 0 {
             i -= 1;
             j -= 1;
-            diff.push(Result::Both(&left[i], &right[j]));
+            diff.push(Result::Both(li.next_back().unwrap(),
+                                   ri.next_back().unwrap()));
         } else {
             break
         }
