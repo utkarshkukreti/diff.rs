8edf9623a450a8574ef76df729a00cc57f1eb449
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2015-04-30 12:24:29 +0530

    feat(slice): add initial (rather inefficient) version

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,39 @@ pub enum Result<T> {
     Both(T, T),
     Right(T)
 }
+
+/// Computes the diff between two slices.
+pub fn slice<'a, T: PartialEq>(left: &'a [T],
+                               right: &'a [T]) -> Vec<Result<&'a T>> {
+    let mut table = vec![vec![0; right.len() + 1]; left.len() + 1];
+
+    for (i, l) in left.iter().enumerate() {
+        for (j, r) in right.iter().enumerate() {
+            table[i+1][j+1] = if l == r {
+                table[i][j] + 1
+            } else {
+                std::cmp::max(table[i][j+1], table[i+1][j])
+            };
+        }
+    }
+
+    let mut diff = vec![];
+    let (mut i, mut j) = (left.len(), right.len());
+    loop {
+        if i > 0 && (j == 0 || table[i][j] == table[i-1][j]) {
+            i -= 1;
+            diff.push(Result::Left(&left[i]));
+        } else if j > 0 && (i == 0 || table[i][j] == table[i][j-1]) {
+            j -= 1;
+            diff.push(Result::Right(&right[j]));
+        } else if i > 0 && j > 0 {
+            i -= 1;
+            j -= 1;
+            diff.push(Result::Both(&left[i], &right[j]));
+        } else {
+            break
+        }
+    }
+    diff.reverse();
+    diff
+}
--- tests/tests.rs
+++ tests/tests.rs
@@ -1,7 +1,62 @@
 #![feature(plugin)]
 #![plugin(speculate)]
 
+extern crate diff;
+
 speculate! {
-    it "works" {
+    describe "slice" {
+        before {
+            fn go<T>(left: &[T], right: &[T], len: usize) where
+                T: Clone + ::std::fmt::Debug + PartialEq
+            {
+                let diff = ::diff::slice(&left, &right);
+                assert_eq!(diff.len(), len);
+                let (mut left_, mut right_) = (vec![], vec![]);
+                for d in diff {
+                    match d {
+                        ::diff::Result::Left(l) => left_.push(l.clone()),
+                        ::diff::Result::Both(l, r) => {
+                            left_.push(l.clone());
+                            right_.push(r.clone());
+                        },
+                        ::diff::Result::Right(r) => right_.push(r.clone()),
+                    }
+                }
+                assert_eq!(left, &left_[..]);
+                assert_eq!(right, &right_[..]);
+            }
+        }
+
+        test "empty slices" {
+            let slice: &[()] = &[];
+            go(&slice, &slice, 0);
+        }
+
+        test "equal + non-empty slices" {
+            let slice = [1, 2, 3];
+            go(&slice, &slice, 3);
+        }
+
+        test "left empty, right non-empty" {
+            let slice = [1, 2, 3];
+            go(&slice, &[], 3);
+        }
+
+        test "left non-empty, right empty" {
+            let slice = [1, 2, 3];
+            go(&[], &slice, 3);
+        }
+
+        test "misc 1" {
+            let left = [1, 2, 3, 4, 1, 3];
+            let right = [1, 4, 1, 1];
+            go(&left, &right, 7);
+        }
+
+        test "misc 2" {
+            let left = [1, 2, 1, 2, 3, 2, 2, 3, 1, 3];
+            let right = [3, 3, 1, 2, 3, 1, 2, 3, 4, 1];
+            go(&left, &right, 14);
+        }
     }
 }
