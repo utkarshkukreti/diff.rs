b9a0ff4723f0a4f81367e7f65ef43f2c8d0af8fe
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2022-04-16 16:39:12 +0530

    use criterion for benchmarks

--- benches/benches.rs
+++ benches/benches.rs
@@ -1,44 +1,44 @@
-#![feature(plugin, test)]
-#![plugin(speculate)]
-
+extern crate criterion;
 extern crate diff;
-extern crate test;
 
-speculate! {
-    describe "slice" {
-        bench "empty" |b| {
-            let slice = [0u8; 0];
-            b.iter(|| ::diff::slice(&slice, &slice));
-        }
+use criterion::Criterion;
 
-        bench "10 equal items" |b| {
-            let slice = [0u8; 10];
-            b.iter(|| ::diff::slice(&slice, &slice));
-        }
+criterion::criterion_group!(benches, bench_slice);
+criterion::criterion_main!(benches);
 
-        bench "10 non-equal items" |b| {
-            let (left, right) = ([0u8; 10], [1u8; 10]);
-            b.iter(|| ::diff::slice(&left, &right));
-        }
+fn bench_slice(c: &mut Criterion) {
+    c.bench_function("empty", |b| {
+        let slice = [0u8; 0];
+        b.iter(|| ::diff::slice(&slice, &slice));
+    });
 
-        bench "100 equal items" |b| {
-            let slice = [0u8; 100];
-            b.iter(|| ::diff::slice(&slice, &slice));
-        }
+    c.bench_function("10 equal items", |b| {
+        let slice = [0u8; 10];
+        b.iter(|| ::diff::slice(&slice, &slice));
+    });
 
-        bench "100 non-equal items" |b| {
-            let (left, right) = ([0u8; 100], [1u8; 100]);
-            b.iter(|| ::diff::slice(&left, &right));
-        }
+    c.bench_function("10 non-equal items", |b| {
+        let (left, right) = ([0u8; 10], [1u8; 10]);
+        b.iter(|| ::diff::slice(&left, &right));
+    });
 
-        bench "1000 equal items" |b| {
-            let slice = [0u8; 1000];
-            b.iter(|| ::diff::slice(&slice, &slice));
-        }
+    c.bench_function("100 equal items", |b| {
+        let slice = [0u8; 100];
+        b.iter(|| ::diff::slice(&slice, &slice));
+    });
 
-        bench "1000 non-equal items" |b| {
-            let (left, right) = ([0u8; 1000], [1u8; 1000]);
-            b.iter(|| ::diff::slice(&left, &right));
-        }
-    }
+    c.bench_function("100 non-equal items", |b| {
+        let (left, right) = ([0u8; 100], [1u8; 100]);
+        b.iter(|| ::diff::slice(&left, &right));
+    });
+
+    c.bench_function("1000 equal items", |b| {
+        let slice = [0u8; 1000];
+        b.iter(|| ::diff::slice(&slice, &slice));
+    });
+
+    c.bench_function("1000 non-equal items", |b| {
+        let (left, right) = ([0u8; 1000], [1u8; 1000]);
+        b.iter(|| ::diff::slice(&left, &right));
+    });
 }
