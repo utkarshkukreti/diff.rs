24ded3702008750f7899d47ab9593715ffc288a3
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2022-04-22 09:49:19 +0530

    add current outputs for sample data as tests to catch future regressions

--- tests/tests.rs
+++ tests/tests.rs
@@ -35,6 +35,11 @@ pub fn undiff_str<'a>(diff: &[::diff::Result<&'a str>]) -> (Vec<&'a str>, Vec<&'
     (left, right)
 }
 
+pub fn undiff_lines<'a>(diff: &[::diff::Result<&'a str>]) -> (String, String) {
+    let (left, right) = undiff_str(diff);
+    (left.join("\n"), right.join("\n"))
+}
+
 pub fn undiff_chars(diff: &[::diff::Result<char>]) -> (String, String) {
     let (mut left, mut right) = (vec![], vec![]);
     for d in diff {
@@ -223,6 +228,77 @@ BacktraceNode {
     ]
 }"#;
             ::diff::lines(actual, expected);
+        }
+    }
+}
+
+#[test]
+fn gitignores() {
+    let all = std::fs::read_to_string("tests/data/gitignores.txt")
+        .unwrap()
+        .split("!!!")
+        .map(|str| str.trim())
+        .filter(|str| !str.is_empty())
+        .map(|str| str.into())
+        .collect::<Vec<String>>();
+
+    go(
+        &all,
+        ::diff::lines,
+        undiff_lines,
+        |d| match d {
+            Left(l) => format!("-{}\n", l),
+            Right(r) => format!("+{}\n", r),
+            Both(l, _) => format!(" {}\n", l),
+        },
+        "tests/data/gitignores.lines.diff",
+    );
+
+    go(
+        &all,
+        ::diff::chars,
+        undiff_chars,
+        |d| match d {
+            Left(l) => format!("[-{}-]", l),
+            Right(r) => format!("{{+{}+}}", r),
+            Both(l, _) => format!("{}", l),
+        },
+        "tests/data/gitignores.chars.diff",
+    );
+
+    fn go<'a, T, Diff, Undiff, ToString>(
+        all: &'a [String],
+        diff: Diff,
+        undiff: Undiff,
+        to_string: ToString,
+        path: &str,
+    ) where
+        Diff: Fn(&'a str, &'a str) -> Vec<::diff::Result<T>>,
+        Undiff: Fn(&[::diff::Result<T>]) -> (String, String),
+        ToString: Fn(&::diff::Result<T>) -> String,
+        T: 'a,
+    {
+        let mut actual = String::new();
+        for i in 0..all.len() {
+            let from = i.saturating_sub(2);
+            let to = (i + 2).min(all.len());
+            for j in from..to {
+                actual.push_str(&format!("i = {}, j = {}\n", i, j));
+                let diff = diff(&all[i], &all[j]);
+                for d in &diff {
+                    actual.push_str(&to_string(d));
+                }
+                actual.push_str("\n");
+                let undiff = undiff(&diff);
+                assert_eq!(&undiff.0, &all[i]);
+                assert_eq!(&undiff.1, &all[j]);
+            }
+        }
+
+        if let Ok(expected) = std::fs::read_to_string(path) {
+            assert_eq!(expected, actual);
+        } else {
+            std::fs::write(path, actual).unwrap();
         }
     }
 }
