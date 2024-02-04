eb843e55bc7d80add5bc0106ccbd329ddbceeb59
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2024-02-04 17:00:35 +0530

    allow diffing using myers with --myers in diff example

--- examples/diff.rs
+++ examples/diff.rs
@@ -1,18 +1,28 @@
 extern crate diff;
 
 fn main() {
-    let args = std::env::args().collect::<Vec<_>>();
+    let mut args = std::env::args().skip(1).collect::<Vec<_>>();
 
-    if args.len() != 3 {
-        println!("usage: cargo run --example diff <first file> <second file>");
+    let myers = args.iter().any(|arg| arg == "--myers");
+
+    args.retain(|arg| arg != "--myers");
+
+    if args.len() != 2 {
+        println!("usage: cargo run --example diff [--myers] <first file> <second file>");
         std::process::exit(1);
     }
 
-    let left = std::fs::read_to_string(&args[1]).unwrap();
-    let right = std::fs::read_to_string(&args[2]).unwrap();
+    let left = std::fs::read_to_string(&args[0]).unwrap();
+    let right = std::fs::read_to_string(&args[1]).unwrap();
 
-    for diff in diff::lines(&left, &right) {
-        match diff {
+    let diff = if myers {
+        diff::myers::lines(&left, &right)
+    } else {
+        diff::lines(&left, &right)
+    };
+
+    for d in diff {
+        match d {
             diff::Result::Left(l) => println!("-{}", l),
             diff::Result::Both(l, _) => println!(" {}", l),
             diff::Result::Right(r) => println!("+{}", r),
