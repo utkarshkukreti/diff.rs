a4dfbfd8b2a8b65c892fd03bb19b13f0a732f8c1
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2024-02-04 17:12:42 +0530

    add char diffing to diff example with --chars

--- examples/diff.rs
+++ examples/diff.rs
@@ -5,7 +5,9 @@ fn main() {
 
     let myers = args.iter().any(|arg| arg == "--myers");
 
-    args.retain(|arg| arg != "--myers");
+    let chars = args.iter().any(|arg| arg == "--chars");
+
+    args.retain(|arg| arg != "--myers" && arg != "--chars");
 
     if args.len() != 2 {
         println!("usage: cargo run --example diff [--myers] <first file> <second file>");
@@ -15,17 +17,47 @@ fn main() {
     let left = std::fs::read_to_string(&args[0]).unwrap();
     let right = std::fs::read_to_string(&args[1]).unwrap();
 
-    let diff = if myers {
-        diff::myers::lines(&left, &right)
-    } else {
-        diff::lines(&left, &right)
-    };
+    if chars {
+        let diff = if myers {
+            diff::myers::chars(&left, &right)
+        } else {
+            diff::chars(&left, &right)
+        };
 
-    for d in diff {
-        match d {
-            diff::Result::Left(l) => println!("-{}", l),
-            diff::Result::Both(l, _) => println!(" {}", l),
-            diff::Result::Right(r) => println!("+{}", r),
+        let mut open = None;
+
+        for d in diff {
+            match (d, open) {
+                (diff::Result::Left(l), Some("-]")) => print!("{}", l),
+                (diff::Result::Left(l), open_) => {
+                    print!("{}[-{}", open_.unwrap_or(""), l);
+                    open = Some("-]");
+                }
+                (diff::Result::Right(r), Some("+}")) => print!("{}", r),
+                (diff::Result::Right(r), open_) => {
+                    print!("{}{{+{}", open_.unwrap_or(""), r);
+                    open = Some("+}");
+                }
+                (diff::Result::Both(l, _), Some(open_)) => {
+                    print!("{}{}", open_, l);
+                    open = None;
+                }
+                (diff::Result::Both(l, _), None) => print!("{}", l),
+            }
+        }
+    } else {
+        let diff = if myers {
+            diff::myers::lines(&left, &right)
+        } else {
+            diff::lines(&left, &right)
+        };
+
+        for d in diff {
+            match d {
+                diff::Result::Left(l) => println!("-{}", l),
+                diff::Result::Both(l, _) => println!(" {}", l),
+                diff::Result::Right(r) => println!("+{}", r),
+            }
         }
     }
 }
